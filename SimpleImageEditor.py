1450b5180e19ec77e537fe63e20b591d7670d4b9
tunahanpnr <b21727652@cs.hacettepe.edu.tr>
2021-12-18 17:04:22 +0300

    change image reader as opencv

--- SimpleImageEditor.py
+++ SimpleImageEditor.py
@@ -1,32 +1,35 @@
+import cv2
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QFileDialog
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
+from PyQt5 import QtGui
 
 import sys
 
 
 class UI(QMainWindow):
-	def __init__(self):
-		super(UI, self).__init__()
+    def __init__(self):
+        super(UI, self).__init__()
 
-		uic.loadUi("openfile.ui", self)
+        uic.loadUi("openfile.ui", self)
 
+        self.button = self.findChild(QPushButton, 'pushButton')
+        self.label = self.findChild(QLabel, 'label')
 
-		self.button = self.findChild(QPushButton, 'pushButton')
-		self.label = self.findChild(QLabel, 'label')
+        self.button.clicked.connect(self.openFileClick)
 
+        self.show()
 
-		self.button.clicked.connect(self.openFileClick)
+    def openFileClick(self):
+        fname = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*);;PNG (*.png);;JPG (*.jpg)")
 
+        if fname:
+            self.image = cv2.imread(fname[0])
+            self.image = QtGui.QImage(self.image.data, self.image.shape[1], self.image.shape[0],
+                                      QtGui.QImage.Format_RGB888).rgbSwapped()
+            self.pixmap = QPixmap(fname[0])
+            self.label.setPixmap(QtGui.QPixmap.fromImage(self.image))
 
-		self.show()
-
-	def openFileClick(self):
-		fname = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*);;PNG (*.png);;JPG (*.jpg)")
-
-		if fname:
-			self.pixmap = QPixmap(fname[0])
-			self.label.setPixmap(self.pixmap)
 
 app = QApplication(sys.argv)
 UIWindow = UI()
