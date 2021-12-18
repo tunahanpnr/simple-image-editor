8a1a230526ac7a3c92427a1fb2c6b733c2e1fc45
tunahanpnr <b21727652@cs.hacettepe.edu.tr>
2021-12-18 20:34:01 +0300

    implement blur image

--- SimpleImageEditor.py
+++ SimpleImageEditor.py
@@ -1,7 +1,6 @@
 import cv2
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QFileDialog
 from PyQt5 import uic
-from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui
 
 import sys
@@ -11,30 +10,43 @@ class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
 
+        self.isBlurred = False
+
         uic.loadUi("openfile.ui", self)
 
-        self.openFileButton = self.findChild(QPushButton, 'openFileButton')
-        self.saveFileButton = self.findChild(QPushButton, 'saveFileButton')
-        self.label = self.findChild(QLabel, 'label')
+        self.openImgButton = self.findChild(QPushButton, 'openImgButton')
+        self.saveImgButton = self.findChild(QPushButton, 'saveImgButton')
+        self.blurImgButton = self.findChild(QPushButton, 'blurImgButton')
 
-        self.openFileButton.clicked.connect(self.openFileClick)
-        self.saveFileButton.clicked.connect(self.saveFileClick)
+        self.openImgButton.clicked.connect(self.openImgClick)
+        self.saveImgButton.clicked.connect(self.saveImgClick)
+        self.blurImgButton.clicked.connect(self.blurImgClick)
+
+        self.label = self.findChild(QLabel, 'label')
 
         self.show()
 
-    def openFileClick(self):
+    def openImgClick(self):
         fname = QFileDialog.getOpenFileName(self, "Open File", "", "All Files(*);;PNG(*.png);;JPG(*.jpg)")
         if fname[0] != '':
             self.image = cv2.imread(fname[0])
             self.q_image = QtGui.QImage(self.image.data, self.image.shape[1], self.image.shape[0],
                                         QtGui.QImage.Format_RGB888).rgbSwapped()
-            self.pixmap = QPixmap(fname[0])
             self.label.setPixmap(QtGui.QPixmap.fromImage(self.q_image))
 
-    def saveFileClick(self):
+    def saveImgClick(self):
         dir_name = QFileDialog.getSaveFileName(self, "Save File", "", "All Files(*);;PNG(*.png);;JPG(*.jpg)")
         if dir_name[0] != '':
             cv2.imwrite(dir_name[0], self.image)
+
+    def blurImgClick(self):
+        self.image = cv2.blur(self.image, (10, 10))
+        self.q_image = QtGui.QImage(self.image.data, self.image.shape[1], self.image.shape[0],
+                                    QtGui.QImage.Format_RGB888).rgbSwapped()
+        self.label.setPixmap(QtGui.QPixmap.fromImage(self.q_image))
+
+
+
 
 
 app = QApplication(sys.argv)
